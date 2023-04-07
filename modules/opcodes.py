2cd063eb41c389a286d9857c46c5738792530476
Lindooren <guojianingkenny@qq.com>
2023-04-07 08:30:52 +0800

    add new opcodes

--- modules/opcodes.py
+++ modules/opcodes.py
@@ -1,7 +1,8 @@
+# collection/classification of several supported opcodes
 class opcodes ():
-    Opcodes = ["LDM", "LDR", "LDD", "LDI", "LDX", "MOV", "STO", "JMP", "CMP", "CMI", "JPE", "JPN", "ADD", "SUB", "INC", "DEC", "OUT", "END", "STX", "AND", "XOR", "OR", "LSL", "LSR", "STI", "STX"]
-    Immediate_Opcodes = ["LDM", "LDR", "CMP", "ADD", "SUB", "AND", "XOR", "OR", "LSL", "LSR"]
-    Direct_Opcodes = ["LDD", "STO", "JMP", "CMP", "JPE", "JPN", "ADD", "SUB", "AND", "XOR", "OR"]
-    Indirect_Opcodes = ["LDI", "CMI", "STI"]
-    Indexed_Opcodes = ["LDX", "STX"]
-    Register_Opcodes = ["Mov", "INC", "DEC"]
+    opcodes = ["LDM", "LDR", "LDD", "LDI", "LDX", "MOV", "STO", "JMP", "CMP", "CMI", "JPE", "JPN", "ADD", "SUB", "INC", "DEC", "OUT", "END", "STX", "AND", "XOR", "OR", "LSL", "LSR", "STI", "STX"]
+    immediate_opcodes = ["LDM", "LDR", "CMP", "ADD", "SUB", "AND", "XOR", "OR", "LSL", "LSR"]
+    direct_opcodes = ["LDD", "STO", "JMP", "CMP", "JPE", "JPN", "ADD", "SUB", "AND", "XOR", "OR"]
+    indirect_opcodes = ["LDI", "CMI", "STI"]
+    indexed_opcodes = ["LDX", "STX"]
+    register_opcodes = ["Mov", "INC", "DEC"]
